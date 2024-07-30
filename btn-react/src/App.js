77ea73f155371518139f2c3be87c4be6323884d6
Oleg <oleg.tcm@gmail.com>
2024-07-30 15:15:41 +0300

    a

---
+++ btn-react/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button> Hello World </Button>
+      </header>
+
+    </div>
+
+  );
+}
+
+export default App;
