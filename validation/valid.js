c27771a653c188c5b8827124b66b764c1210effa
Oleg <oleg.tcm@gmail.com>
2024-08-05 17:30:01 +0300

    a

--- validation/valid.js
+++ validation/valid.js
@@ -1,14 +1,16 @@
-const emailInput = document.getElementById('emailInput');
-const emailCheker = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+function validateEmail() {
+    const emailInput = document.getElementById('emailInput');
+    const emailCheker = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
-emailInput.addEventListener('input', () => {
-    const isValid = emailCheker.test(emailInput.value);
-    if (!isValid) {
-        emailInput.setCustomValidity('Введите корректный емайл адрес');
-    } else {
-        emailInput.setCustomValidity('');
-    }
-});
+    emailInput.addEventListener('input', () => {
+        const isValid = emailCheker.test(emailInput.value);
+        if (!isValid) {
+            emailInput.setCustomValidity('Введите корректный емайл адрес');
+        } else {
+            emailInput.setCustomValidity('');
+        }
+    });
+}
 
 function validatePassword() {
     const checkPassword = new RegExp('^[a-zA-Z0-9_.-]*$')
@@ -25,4 +27,21 @@ function validatePassword() {
         }
     });
 }
-validatePassword()
+
+function checkFields() {
+    const form = document.getElementById("formSend");
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+
+        let email = document.getElementById("emailInput").value;
+        let password = document.getElementById("inputPassword").value;
+        if (email === "" || password === "") alert("Поля пустые");
+        else form.submit();
+    });
+}
+
+checkFields();
+validateEmail();
+validatePassword();
